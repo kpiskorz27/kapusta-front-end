2a88aa975190da70ccc5421628c8afa3be3a1231
kpiskorz27 <kpiskorz@gmx.at>
2024-10-01 15:37:38 +0200

    index.js store.js App.js duplicates remove

--- src/redux/slices/userOperations.js
+++ src/redux/slices/userOperations.js
@@ -1,15 +1,27 @@
-import axios from "axios";
+import axios from "../../api/api";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import authSelectors from "./authSelectors";
 
 const getUser = createAsyncThunk(
   "user/getUser",
-  async (_, { rejectWithValue }) => {
+  async (_, { getState, rejectWithValue }) => {
+    const state = getState();
+    const token = authSelectors.getAccessToken(state);
+
+    console.log("Fetching user data with token:", token);
+
     try {
-      const { data } = await axios.get("/user");
+      const { data } = await axios.get("/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("User data received:", data);
       return data.data;
     } catch (error) {
+      console.error("Error fetching user data:", error.response?.data);
       return rejectWithValue(
-        error.response.data.message || "Failed to fetch user data"
+        error.response?.data?.message || "Failed to fetch user data"
       );
     }
   }
--- src/redux/storeM.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducersM";
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-export default store;
