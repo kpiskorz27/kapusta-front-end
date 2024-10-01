3c5f003221c45eda99603b7a1de957bf38e68d5b
kpiskorz27 <kpiskorz@gmx.at>
2024-10-01 16:14:02 +0200

    Update BudgetSummary.jsx

--- src/components/BudgetSummary.jsx
+++ src/components/BudgetSummary.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const BudgetSummary = ({ income = 0, expenses = 0 }) => {
-  console.log("Expenses in BudgetSummary:", expenses); // Check if data is correctly passed
-
   return (
     <div className="budget-summary-container">
       <div className="expenses">
